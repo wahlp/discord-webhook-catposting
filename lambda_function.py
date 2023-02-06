3262569df3315335c66856f82626bf9ccdc3015d
wahlp <wahlp322@gmail.com>
2023-02-07 03:49:44 +0800

    Change webhook data

--- lambda_function.py
+++ lambda_function.py
@@ -1,19 +1,23 @@
 import json
 import os
-import random
 import urllib3
+from datetime import datetime
 
 http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
-    s = f"Hello, your lucky number is {random.randint(0, 99)}"
+    d0 = datetime(2023, 1, 19)
+    d1 = datetime.today()
+    delta = d1 - d0
+    s = f"day {delta.days} of no danielle newjeans gf"
     send_webhook(s)
 
 def send_webhook(content: str):
     url = os.environ['WEBHOOK_URL']
     msg = {
-        "username": "Lucky Number Bot",
+        "username": "tfw no gf",
         "content": content,
+        "avatar_url": "https://cdn.discordapp.com/attachments/342727918679490561/1072240877021433856/image.png"
     }
 
     encoded_msg = json.dumps(msg).encode("utf-8")
