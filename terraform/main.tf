141c96b48ef30ccb517452482b3f5767e5fa398c
wahlp <wahlp322@gmail.com>
2023-05-02 00:33:23 +0800

    Change lambda timeout

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,6 +58,7 @@ resource "aws_lambda_function" "lambda" {
   role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "lambda_function.lambda_handler"
   runtime = "python3.9"
+  timeout = 10
   
   environment {
     variables = {
